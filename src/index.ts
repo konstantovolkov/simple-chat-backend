db2f1235595f82d1bb1ee0fa777787ec89f8da57
konstantovolkov <konstantovolkov.1@yandex.ru>
2022-05-09 20:13:57 +0300

    add track and terminate broken connections

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
 import { WebSocketServer, WebSocket } from 'ws';
 import { v4 as uuidv4 } from 'uuid';
 
-const wss = new WebSocketServer({
-  port: parseInt(process.env.PORT || ''),
-  path: '/chat'
-});
+const PING_INTERVAL = 5000;
+
+interface PingableWebSocket extends WebSocket {
+  isAlive?: boolean;
+}
 
 const history: { text: string; id: string }[] = [];
 
 interface ClientData {
   message: string;
 }
+
+
+const wss = new WebSocketServer<PingableWebSocket>({
+  port: parseInt(process.env.PORT || '8080'),
+  path: '/chat'
+});
+
+const pingTimer = setInterval(() => {
+  wss.clients.forEach(client => {
+    if (!client.isAlive) {
+      return client.terminate();
+    }
+
+    client.isAlive = false;
+    client.ping();
+  })
+}, PING_INTERVAL)
 
 wss.on('connection', (clientWs) => {
   clientWs.on('message', (data) => {
@@ -35,14 +53,19 @@ wss.on('connection', (clientWs) => {
     })
   });
 
-  history.push({
-    text: 'Welcome to the chat!',
-    id: uuidv4(),
-  })
-
   clientWs.send(JSON.stringify({
     data: {
       history
     }
   }))
+
+  clientWs.isAlive = true;
+
+  clientWs.on('pong', () => {
+    clientWs.isAlive = true;
+  })
 });
+
+wss.on('close', () => {
+  clearInterval(pingTimer);
+})
